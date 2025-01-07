23ed8c12d9c67ac5d1b6ece28c18a1cde5c123bc
Igor Kirpichnikov <rigor652024@outlook.com>
2025-01-07 21:43:20 +0300

    Add files via upload

---
+++ exercise-02.py
@@ -0,0 +1,9 @@
+# Практическое задание по уроку "Базовые структуры данных". Задача 1, 2, 3, 4:
+print(9**0.5*5) # 1st program
+print(9.99 > 9.98 and 1000 != 1000.1) # 2nd program
+print(2 * 2 + 2)  # 3rd program
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+str_a = '123.456' # 4th program
+b = int(float(str_a) * 10)
+print(b % 10)
